a1ead0eaa9e2e230e8d4f86dbcc9e88dd974c117
Mohamed Azeem B <mohamedazeem.b@cognizant.com>
2017-03-28 19:48:13 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Author : Mohamed Azeem
+Email  : azeem.fx@gmail.com
+
+# About This Application : Learn Technology By Viewing Each Cards
+
+## Build & development
+
+Run `grunt` for building and `grunt serve` for preview.
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
---
+++ pub/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name learnTechByCardsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the learnTechByCardsApp
+ */
+angular.module('learnTechByCardsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
