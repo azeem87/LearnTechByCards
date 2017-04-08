a405b2dea9e2e8882def38f719dd1cd252ee8e56
Mohamed Azeem B <mohamedazeem.b@cognizant.com>
2017-04-08 20:01:34 +0530

    additional load npm tasks added

--- Gruntfile.js
+++ Gruntfile.js
@@ -374,8 +374,16 @@ module.exports = function (grunt) {
     }
   });
 
+  //Load NPM tasks
   grunt.loadNpmTasks('grunt-nodemon');
   grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-env');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-filerev');
 
   grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
     if (target === 'prod') {
